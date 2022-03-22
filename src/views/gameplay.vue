24f3fffa639113530276c88683ab0988250784ba
pietro445 <pepe.polak2004@gmail.com>
2022-03-22 15:00:47 +0100

    petov prvy commit

---
+++ src/views/gameplay.vue
@@ -0,0 +1,12 @@
+<template>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <header class="items-center">
+    <h1>Karaoke</h1>
+  </header>
+    <body>
+      <div>serus</div>
+    </body>
+</template>
