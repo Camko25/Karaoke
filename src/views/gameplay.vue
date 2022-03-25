dcda68aca6e949f380c3e68e5ec4ac190b6ae243
adv3ntur3ss <xox.svk@gmail.com>
2022-03-25 12:55:07 +0100

    a

--- src/views/gameplay.vue
+++ src/views/gameplay.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <div class="place-content-center">
-      <button class="border-slate-200">
-Aaaaaaaa
-      </button>
+      <button class="border-slate-200">Aaaaaaaa</button>
     </div>
   </div>
 </template>
