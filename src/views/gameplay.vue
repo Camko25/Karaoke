a83d35ab607ef03debc9720e255a6717c631ff09
adv3ntur3ss <xox.svk@gmail.com>
2022-03-25 12:27:53 +0100

    tailwind

--- src/views/gameplay.vue
+++ src/views/gameplay.vue
@@ -1,12 +1,9 @@
 <template>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <header class="items-center">
-    <h1>Karaoke</h1>
-  </header>
-    <body>
-      <div>serus</div>
-    </body>
+  <div>
+    <div class="place-content-center">
+      <button class="border-slate-200">
+Aaaaaaaa
+      </button>
+    </div>
+  </div>
 </template>
