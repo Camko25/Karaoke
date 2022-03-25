5546035bffa75da62e166b11086b98a16578d82a
adv3ntur3ss <xox.svk@gmail.com>
2022-03-25 13:57:55 +0100

    tailwind_ide?

--- src/views/gameplay.vue
+++ src/views/gameplay.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>
-    <div class="place-content-center">
-      <button class="border-slate-200">Aaaaaaaa</button>
+  <div class="bg-black relative rounded-xl overflow-auto p-8">
+    <div
+      class="grid grid-cols-3 gap-4 place-iems-stretch font-mono text-white text-sm font-bold leading-6"
+    >
+      <div
+        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
+      >
+        NEVIEM
+      </div>
+      <div class="bg-stripes-purple rounded-lg grid">
+        <div
+          class="w-14 h-14 rounded-lg flex items-center justify-center bg-purple-500 shadow-lg place-self-start"
+        >
+          CI
+        </div>
+      </div>
+      <div
+        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
+      >
+        TO
+      </div>
+      <div
+        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
+      >
+        FUNGUJE
+      </div>
+      <div
+        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
+      >
+        ALE
+      </div>
+      <div
+        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
+      >
+        ASI-ANO
+      </div>
     </div>
   </div>
 </template>
