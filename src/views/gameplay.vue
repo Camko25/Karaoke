79301678753e0cd8974ef177541c3d8299c15763
adv3ntur3ss <xox.svk@gmail.com>
2022-03-28 12:52:48 +0200

    countdown

--- src/views/gameplay.vue
+++ src/views/gameplay.vue
@@ -1,40 +1,46 @@
 <template>
-  <div class="bg-black relative rounded-xl overflow-auto p-8">
-    <div
-      class="grid grid-cols-3 gap-4 place-iems-stretch font-mono text-white text-sm font-bold leading-6"
-    >
-      <div
-        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
-      >
-        NEVIEM
-      </div>
-      <div class="bg-stripes-purple rounded-lg grid">
-        <div
-          class="w-14 h-14 rounded-lg flex items-center justify-center bg-purple-500 shadow-lg place-self-start"
-        >
-          CI
-        </div>
-      </div>
-      <div
-        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
-      >
-        TO
-      </div>
-      <div
-        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
-      >
-        FUNGUJE
-      </div>
-      <div
-        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
-      >
-        ALE
-      </div>
-      <div
-        class="p-8 rounded-lg flex items-center justify-center bg-purple-300 dark:bg-purple-800 dark:text-purple-400"
-      >
-        ASI-ANO
-      </div>
-    </div>
+  <div class="container">
+    <div class="">{{ timerCount }}</div>
+    <div>HRACIA PLOCHA</div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeValue()">start</button>
   </div>
 </template>
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+                timerCount: null
+            }
+        },
+
+        watch: {
+
+            timerCount: {
+                handler(value) {
+
+                    if (value > 0) {
+                        setTimeout(() => {
+                            this.timerCount--;
+                        }, 1000);
+                    }
+                    if (value == 0){
+                      this.timerCount= null
+                      // gamePlay()  toto bude callovat celu tu hru
+                    }
+
+                },
+                deep: true
+            }
+
+        },
+        methods:{
+          changeValue(){
+            this.timerCount = 5
+          }
+        },
+    }
+
+</script>
