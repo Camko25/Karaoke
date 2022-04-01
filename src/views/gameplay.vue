26505ce09f05db561e2f899d5b92623cc3f5babc
Coolguyname <adricnkucera@gmail.com>
2022-04-01 11:59:09 +0200

    UI completed ado

--- src/views/gameplay.vue
+++ src/views/gameplay.vue
@@ -1,42 +1,76 @@
 <template>
   <body class="m-0">
     <div class="container w-screen h-screen font-sans">
-      <header class="w-screen h-12 bg-gray-400 box-border shadow-2xl">
-        <div class="h-12 w-24">
-          <h4 class="text-center p-3">Logo</h4>
-        </div>
-      </header>
+      
       <section
-        class="rgb-background w-screen h-[577px] flex flex-col box-border"
+        class="rgb-background w-screen h-screen flex flex-col"
       >
+        <header class="w-screen h-12 bg-gray-400 shadow-2xl">
+         <div class="h-12 w-24">
+           <h4 class="text-center p-3">Logo</h4>
+          </div>
+       </header>
         <div  class="w-screen h-[150px] py-9 text-center font-semibold text-7xl">
           {{ timerCount }}
         </div>
+
         <div class="w-screen h-[240px] bg-stone-800 flex flex-col">
+
+          <div
+            class="h-80 w-6 bg-cyan-300 absolute opacity-75 left-48 top-[160px] rounded-lg"
+          ></div>
+
           <div
             id="string"
-            class="h-[8px] w-screen bg-slate-300 my-[16px]"
-          ></div>
+            class="h-[24px] w-screen my-[8px] py-2 relative"
+          > 
+            <div class=" h-[8px] w-screen bg-slate-300 "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[80vw] rounded-xl "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[60vw] rounded-xl "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[40vw] rounded-xl "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[20vw] rounded-xl "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[0vw] rounded-xl "></div>
+          </div>
+
           <div
             id="string"
-            class="h-[8px] w-screen bg-slate-300 my-[16px]"
-          ></div>
+            class="h-[24px] w-screen my-[8px] py-2 relative"
+          > 
+            <div class=" h-[8px] w-screen bg-slate-300 "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[95vw] rounded-xl "></div>
+          </div>
+
           <div
             id="string"
-            class="h-[8px] w-screen bg-slate-300 my-[16px]"
-          ></div>
+            class="h-[24px] w-screen my-[8px] py-2 relative"
+          > 
+            <div class=" h-[8px] w-screen bg-slate-300 "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[40vw] rounded-xl "></div>
+          </div>
+
           <div
             id="string"
-            class="h-[8px] w-screen bg-slate-300 my-[16px]"
-          ></div>
+            class="h-[24px] w-screen my-[8px] py-2 relative"
+          > 
+            <div class=" h-[8px] w-screen bg-slate-300 "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl "></div>
+          </div>
+
           <div
             id="string"
-            class="h-[8px] w-screen bg-slate-300 my-[16px]"
-          ></div>
+            class="h-[24px] w-screen my-[8px] py-2 relative"
+          > 
+            <div class=" h-[8px] w-screen bg-slate-300 "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl "></div>
+          </div>
+
           <div
             id="string"
-            class="h-[8px] w-screen bg-slate-300 my-[16px]"
-          ></div>
+            class="h-[24px] w-screen my-[8px] py-2 relative"
+          > 
+            <div class=" h-[8px] w-screen bg-slate-300 "></div>
+            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl "></div>
+          </div>
         </div>
         <!-- <div class="h-20 py-4 mx-auto ">
                 <div class=" bg-white w-20 h-14 py-3 text-center text-2xl font-medium text-black rounded-3xl">69</div>
