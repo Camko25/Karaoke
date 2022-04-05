b7213df12bc0fdf24ecf383ae312a89e58a055cb
adv3ntur3ss <xox.svk@gmail.com>
2022-04-05 18:40:26 +0200

    pohyb not

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,3 @@ export default {
   components: { gameplay },
 };
 </script>
-
-<style>
-#app{
-  font-family: Arial, Helvetica, sans-serif -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  background-color: black;
-  color: white;
-  height: 100vh;
-  width: 100vw;
-}
-</style>
--- src/views/gameplay.vue
+++ src/views/gameplay.vue
@@ -1,75 +1,74 @@
 <template>
   <body class="m-0">
     <div class="container w-screen h-screen font-sans">
-      
-      <section
-        class="rgb-background w-screen h-screen flex flex-col"
-      >
+      <section class="rgb-background w-screen h-screen flex flex-col">
         <header class="w-screen h-12 bg-gray-400 shadow-2xl">
-         <div class="h-12 w-24">
-           <h4 class="text-center p-3">Logo</h4>
+          <div class="h-12 w-24">
+            <h4 class="text-center p-3">Logo</h4>
           </div>
-       </header>
-        <div  class="w-screen h-[150px] py-9 text-center font-semibold text-7xl">
+        </header>
+        <div class="w-screen h-[150px] py-9 text-center font-semibold text-7xl">
           {{ timerCount }}
         </div>
 
         <div class="w-screen h-[240px] bg-stone-800 flex flex-col">
-
           <div
             class="h-80 w-6 bg-cyan-300 absolute opacity-75 left-48 top-[160px] rounded-lg"
           ></div>
 
-          <div
-            id="string"
-            class="h-[24px] w-screen my-[8px] py-2 relative"
-          > 
-            <div class=" h-[8px] w-screen bg-slate-300 "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[80vw] rounded-xl "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[60vw] rounded-xl "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[40vw] rounded-xl "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[20vw] rounded-xl "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[0vw] rounded-xl "></div>
+          <div id="string" class="h-[24px] w-screen my-[8px] py-2 relative">
+            <div class="h-[8px] w-screen bg-slate-300"></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[80vw] rounded-xl"
+            ></div>
+            <div
+              id="movin"
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[60vw] rounded-xl"
+            ></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[40vw] rounded-xl"
+            ></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[20vw] rounded-xl"
+            ></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[0vw] rounded-xl"
+            ></div>
           </div>
 
-          <div
-            id="string"
-            class="h-[24px] w-screen my-[8px] py-2 relative"
-          > 
-            <div class=" h-[8px] w-screen bg-slate-300 "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[95vw] rounded-xl "></div>
+          <div id="string" class="h-[24px] w-screen my-[8px] py-2 relative">
+            <div class="h-[8px] w-screen bg-slate-300"></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[95vw] rounded-xl"
+            ></div>
           </div>
 
-          <div
-            id="string"
-            class="h-[24px] w-screen my-[8px] py-2 relative"
-          > 
-            <div class=" h-[8px] w-screen bg-slate-300 "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[40vw] rounded-xl "></div>
+          <div id="string" class="h-[24px] w-screen my-[8px] py-2 relative">
+            <div class="h-[8px] w-screen bg-slate-300"></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[40vw] rounded-xl"
+            ></div>
           </div>
 
-          <div
-            id="string"
-            class="h-[24px] w-screen my-[8px] py-2 relative"
-          > 
-            <div class=" h-[8px] w-screen bg-slate-300 "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl "></div>
+          <div id="string" class="h-[24px] w-screen my-[8px] py-2 relative">
+            <div class="h-[8px] w-screen bg-slate-300"></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl"
+            ></div>
           </div>
 
-          <div
-            id="string"
-            class="h-[24px] w-screen my-[8px] py-2 relative"
-          > 
-            <div class=" h-[8px] w-screen bg-slate-300 "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl "></div>
+          <div id="string" class="h-[24px] w-screen my-[8px] py-2 relative">
+            <div class="h-[8px] w-screen bg-slate-300"></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl"
+            ></div>
           </div>
 
-          <div
-            id="string"
-            class="h-[24px] w-screen my-[8px] py-2 relative"
-          > 
-            <div class=" h-[8px] w-screen bg-slate-300 "></div>
-            <div class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl "></div>
+          <div id="string" class="h-[24px] w-screen my-[8px] py-2 relative">
+            <div class="h-[8px] w-screen bg-slate-300"></div>
+            <div
+              class="h-8 w-12 bg-green-400 absolute bottom-[-4px] left-[90vw] rounded-xl"
+            ></div>
           </div>
         </div>
         <!-- <div class="h-20 py-4 mx-auto ">
@@ -79,15 +78,35 @@
           <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             v-if="!isHidden"
-            @click="changeValue(), isHidden = true"
+            @click="changeValue(), (isHidden = true)"
           >
             start
           </button>
         </div>
+        <div>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            @click="(hidden = 0), recordAudio()"
+          >
+            RECORD
+          </button>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            v-if="hidden == 0"
+            @click="(hidden = 1), stop()"
+          >
+            STOPRECORDING
+          </button>
+          <button
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounder"
+            @click="stopListening()"
+          >
+            STOPLISTENING
+          </button>
+        </div>
       </section>
-  <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="recordAudio()">record</button>
+      <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="recordAudio()">record</button>
   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="stopRecording()">stopRecording</button> -->
-
     </div>
   </body>
 </template>
@@ -100,8 +119,12 @@ export default {
     return {
       timerCount: null,
       recorder: null,
+      chunks: [],
+      hidden: 1,
+      device: null,
+      blobObj: null,
       starter: false,
-    }
+    };
   },
 
   watch: {
@@ -109,59 +132,78 @@ export default {
       handler(value) {
         if (value > 0) {
           setTimeout(() => {
-            this.timerCount--
+            this.timerCount--;
           }, 1000);
         }
         if (value == 0) {
-          this.timerCount = null
+          this.timerCount = null;
         }
-        if (this.timerCount == null){
-        this.gamePlay()
-      }
-        console.log(this.timerCount)
+        if (this.timerCount == null) {
+          this.gamePlay();
+        }
+        console.log(this.timerCount);
       },
       deep: true,
     },
   },
   methods: {
     changeValue() {
-      this.timerCount = 5
-
+      this.timerCount = 5;
     },
-    gamePlay(){
-        this.playAudio() 
+    gamePlay() {
+      this.playAudio();
+      this.goMove();
     },
-    playAudio(){
-      var audio = new Audio(require('../audio/Guitar Chords.mp3'))
-      audio.play()
-    }
-    // recordAudio() {
-    //   var device = navigator.mediaDevices.getUserMedia({ audio: true })
-    //   var items = []
-    //   device.then((stream) => {
-    //     this.recorder = new MediaRecorder(stream)
-    //     this.recorder.ondataavailable = (e) => {
-    //       items.push(e.data)
-    //       if (recorder.state == "inactive") {
-    //         var blob = new Blob(items, { type: "audio/*" })
-    //         var audio = document.getElementById("audio")
-    //         var mainaudio = document.createElement("audio")
-    //         mainaudio.setAttribute("controls", "controls")
-    //         audio.appendChild(mainaudio)
-    //         mainaudio.innerHTML =
-    //           '<source src="' +
-    //           URL.createObjectURL(blob) +
-    //           '" type="audio/*" />'
-    //       }
-    //     }
-    //     this.recorder.start()
-    //     stopRecording(); {
-    //       this.recorder.stop()
-    //     }
-    //   })
-    // },
+    playAudio() {
+      var audio = new Audio(require("../assets/audio/Guitar Chords.mp3"));
+      audio.play();
+    },
+    recordAudio() {
+      this.device = navigator.mediaDevices.getUserMedia({ audio: true });
+      setInterval(function () {}, 5000);
+      this.device.then((stream) => {
+        this.recorder = new MediaRecorder(stream);
+        this.recorder.ondataavailable = (e) => {
+          this.chunks.push(e.data);
+          if (this.recorder.state == "inactive") {
+            let blob = new Blob(this.chunks, { type: "audio/wav" });
+            // save to blobObj
+            this.blobObj = blob;
+            this.chunks = [];
+            // emit to parent
+            this.$emit("send-audio", this.blobObj);
+            this.blobObj = null;
+          }
+        };
+        // start
+        this.recorder.start();
+      });
+      setInterval(() => {
+        console.log(this.recorder.state);
+      }, 2000);
+    },
+    stop() {
+      // stop
+      this.recorder.stop();
+    },
+    stopListening() {
+      this.recorder.getAudioTracks().forEach((track) => {
+        track.stop();
+      });
+    },
+    goMove() {
+      console.log("bezi");
+      let x = 97;
+      let square = document.getElementById("movin");
+      if (x > -1) {
+        setInterval(() => {
+          x--;
+          square.style.left = x + "vw";
+        }, 200);
+      }
+    },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -173,5 +215,9 @@ export default {
     #96068e,
     #6e0660
   );
+}
+#movin {
+  position: absolute;
+  left: 97vw;
 }
 </style>
