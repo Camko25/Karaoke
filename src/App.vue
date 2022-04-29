7998ede461ff4caed98a37b11ded9df78d1312d2
Samuel Sladky <samuel.sladky@gmail.com>
2022-04-29 11:31:59 +0200

    updated logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <gameplay />
+    <gameResult />
   </div>
 </template>
 
 <script>
 import gameplay from "@/views/gameplay.vue";
 
+
+
 export default {
-  components: { gameplay },
+  components: { gameplay},
 };
 </script>
